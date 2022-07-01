63f23958edc424cb53754e48590fdc40ee865504
ZainSaleem7 <98874779+ZainSaleem7@users.noreply.github.com>
2022-07-01 19:29:36 +0500

    add 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,18 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+`
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
-  }
+    return (
+        <Box>
+            <h1>404 - Page Not Found</h1>
+        </Box>
+    )
+}
